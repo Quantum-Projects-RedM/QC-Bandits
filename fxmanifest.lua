123ee16bbf612c61191ab59107f0175ae600ab09
Artmines <96462463+Artmines@users.noreply.github.com>
2025-01-10 19:48:25 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+author 'RexShack#3041'
+
+quantum_github 'https://github.com/Quantum-Projects-RedM/QC-Bandits'
+description 'RSG Bandits.'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua',
+}
+
+dependency 'rsg-core'
+
+lua54 'yes'
